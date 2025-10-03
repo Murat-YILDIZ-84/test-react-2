e2844c9a2b6e4b3ba760b4589e712e4d57e2d4c8
Coders Club <hi@codersclub.com>
2025-10-03 17:55:20 +0300

    commit-29

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import axios from "axios";
-import './index.css';
+import React from 'react'
+import ReactPlayer from "react-player";
 
 function Procedures(){
-  const adres = "https://test-1-k2ol.onrender.com";
-  
-  const [result, setResult] = useState(["Sunucu Bekleniyor..."]);
-
-  function Test(){
-    axios(adres + "/sunucu")
-    .then((res)=> setResult(res.data))
-    .catch((e)=> setResult("Sunucudan yanıt alınamadı\r\n" + e));
-  }
-
   return (
     <>
-      <button onClick={Test}>Test</button>
-
-      <script type="text/javascript" src="https://istanbuluseyret.ibb.gov.tr/wp-content/plugins/bradmax-player/assets/js/default_player.js" id="bradmax-player-js"></script>
-      <div id="bradmaxPlayer" class='playerArea'></div>
-      {result}
-      
+      <ReactPlayer src='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
     </>
   );
 }
